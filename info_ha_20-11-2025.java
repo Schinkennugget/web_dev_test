876e54d681d0c8c2c0bc5cb2e08e76a06408f1ad
Schinkennugget <168964051+Schinkennugget@users.noreply.github.com>
2025-11-26 16:25:48 +0100

    Info 1

---
+++ info_ha_20-11-2025.java
@@ -0,0 +1,9 @@
+void breitensuche(String startKnoten, String endKnoten) {
+    ArrayList<Knoten> warteliste = new ArrayList<Knoten>();
+    ArrayList<Knoten> fertig = new ArrayList<Knoten>();
+
+    while (!fertig.get(fertig.size() - 1).bezeichnerGeben().equals(endKnoten)) {
+        
+    }
+    
+}
