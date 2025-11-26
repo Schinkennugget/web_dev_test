0b61318895cab2eda09641a2ff946ff18ddf1e2d
Schinkennugget <168964051+Schinkennugget@users.noreply.github.com>
2025-11-26 21:09:58 +0100

    Info 3

--- info_ha_20-11-2025.java
+++ info_ha_20-11-2025.java
@@ -1,9 +1,38 @@
 void breitensuche(String startKnoten, String endKnoten) {
-    ArrayList<Knoten> warteliste = new ArrayList<Knoten>();
-    ArrayList<Knoten> fertig = new ArrayList<Knoten>();
+    ArrayList<String> warteliste = new ArrayList<String>();
+    ArrayList<String> fertig = new ArrayList<String>();
+    String aktuellerKnotenBezeichner = startKnoten;
 
+    //Solange der gesuchte Knoten nicht abgearbeitet wurde
     while (!fertig.get(fertig.size() - 1).bezeichnerGeben().equals(endKnoten)) {
+        //Schauen, ob es eine Kante zwischen dem aktuellen Knoten und einem verbundenen gibt,
+        //die noch nicht in der Fertig-Liste sind
+
+
+        //Der aktuelle Knoten mit allen Kanten, wenn keine da ist, -1
+        ArrayList<Integer> kantenVonAktuellemKnoten = matrix.get(knotenBezeichnerGeben(aktuellerKnotenBezeichner));
         
+        //Checkt alle Kanten
+        for (int i = 0; i < kantenVonAktuellemKnoten.size(); i++) {
+            //Wenn eine Kante da ist
+            if (kantenVonAktuellemKnoten.get(i) > -1) {
+                String andererKnotenBezeichner = knotenBezeichnerGeben(i);
+                //Wenn der Knoten, zu dem die Kante noch gehört, noch nicht in der Warte-
+                //oder fertig-Liste ist
+                if (!fertig.contains(andererKnotenBezeichner) && !warteliste.contains(andererKnotenBezeichner)) {
+                    //Knoten zur Warteliste hinzufügen
+                    warteliste.add(andererKnotenBezeichner);
+                }
+            }
+        }
+
+        fertig.add(aktuellerKnotenBezeichner);
+        if (warteliste.size() > 0) {
+            aktuellerKnotenBezeichner = warteliste.get(0);
+            warteliste.remove(0);
+        } else {
+            //Die Warteliste ist leer, alles wurde abgearbeitet
+            break;
+        }
     }
-    
 }
